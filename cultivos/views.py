0ccc1858771c4961321f676608fcbab2e0a35bf1
victor <victor.renier.marin@gmail.com>
2025-08-29 15:15:24 -0400

    detalle cultivos

--- cultivos/views.py
+++ cultivos/views.py
@@ -24,5 +24,36 @@ def listar_cultivos(request):
     }
     return render(request, 'cultivos/lista_cultivos.html', contexto)
 
-def detalle_cultivo(request):
-    pass
+def detalle_cultivo(request, nombre):
+    cultivos = {
+        'Tomate': {
+            'tipo':'fruto',
+            'descripcion': 'Requiere sol directo y riego moderado, muy sensible al frio',
+            'siembra':'Agosto - Octubre',
+            'cosecha':'Noviembre - Enero'
+        },
+        'Lechuga': {
+            'tipo':'hoja',
+            'descripcion': 'Preferentemente sombra y suelo humedo',
+            'siembra':'Marzo - Mayo',
+            'cosecha':'60 días después de sembrado'
+        },
+        'Albahaca': {
+            'tipo':'hoja',
+            'descripcion': 'Prefiere climas cálidos, no tolerancia a las heladas',
+            'siembra':'Septiembre - Noviembre',
+            'cosecha':'40 dias después de la siembra'
+        },
+    }
+    cultivos = cultivos.get(nombre)
+    if not cultivos:
+        return render(request, 'cultivos/no_encontrado.html', {'nombre':nombre})
+    contexto = {
+        'nombre':nombre,
+        'tipo':cultivos['tipo'],
+        'descripcion':cultivos['descripcion'],
+        'siembra':cultivos['siembra'],
+        'cosecha':cultivos['cosecha'],
+    }
+    return render(request, 'cultivos/detalle.html', contexto)
+    
