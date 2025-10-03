cc482ece22799fcabb2d7ef2a4ee67c7bbdf57c6
IVY ANAYA PRADINES GUZMAN <ivy.pradines@inacapmail.cl>
2025-10-03 15:09:47 -0300

    CositasCositas

--- cultivos/urls.py
+++ cultivos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import listar_cultivos, detalle_cultivo
+from .views import listar_cultivos, detalle_cultivo, registrar_cultivo
 
 urlpatterns = [
     path('', listar_cultivos, name='listar_cultivos'),
+    path('agregar/', registrar_cultivo, name='registrar_cultivo'),
     path('detalle/<str:nombre>/', detalle_cultivo, name='detalle_cultivo'),
 ]
--- cultivos/views.py
+++ cultivos/views.py
@@ -1,59 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CultivoForm
+from .models import Cultivo
 
+def registrar_cultivo(request):
+    if request.method == 'POST':
+        form = CultivoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_cultivos') #Nombre URL del listado
+    else:
+        form = CultivoForm()
+    return render(request, 'cultivos/registrar_cultivo.html', {'form':form})
+        
 def listar_cultivos(request):
-    cultivos_originales = [
-        {'nombre': 'Tomate', 'tipo': 'fruto'},
-        {'nombre': 'Lechuga', 'tipo': 'hoja'},
-        {'nombre': 'Albahaca', 'tipo': 'hoja'},
-        {'nombre': 'Zanahoria', 'tipo': 'raiz'},
-        {'nombre': 'Cilantro', 'tipo': 'hoja'},
-        {'nombre': 'Pimiento', 'tipo': 'fruto'},
-    ]
-    termino = request.GET.get('buscar', '')
-    tipo = request.GET.get('tipo', '')
-    cultivo_filtrados = cultivos_originales
-    if termino:
-        cultivo_filtrados = [c for c in cultivo_filtrados if termino.lower() in c['nombre'].lower()]
-    if tipo:
-        cultivo_filtrados = [c for c in cultivo_filtrados if c['tipo'] == tipo]
-
-    contexto = {
-        'titulo':'Mi huerto',
-        'mensaje':'Filtra los cultivos por nombre y tipo',
-        'cultivos': cultivo_filtrados,
-    }
-    return render(request, 'cultivos/lista_cultivos.html', contexto)
+    cultivos = Cultivo.objects.all()
+    return render(request, 'cultivos/lista_cultivos.html',{
+        'titulo': 'Listado de Cultivos',
+        'cultivos':cultivos
+    })
 
 def detalle_cultivo(request, nombre):
-    cultivos = {
-        'Tomate': {
-            'tipo':'fruto',
-            'descripcion': 'Requiere sol directo y riego moderado, muy sensible al frio',
-            'siembra':'Agosto - Octubre',
-            'cosecha':'Noviembre - Enero'
-        },
-        'Lechuga': {
-            'tipo':'hoja',
-            'descripcion': 'Preferentemente sombra y suelo humedo',
-            'siembra':'Marzo - Mayo',
-            'cosecha':'60 días después de sembrado'
-        },
-        'Albahaca': {
-            'tipo':'hoja',
-            'descripcion': 'Prefiere climas cálidos, no tolerancia a las heladas',
-            'siembra':'Septiembre - Noviembre',
-            'cosecha':'40 dias después de la siembra'
-        },
-    }
-    cultivos = cultivos.get(nombre)
-    if not cultivos:
-        return render(request, 'cultivos/no_encontrado.html', {'nombre':nombre})
-    contexto = {
-        'nombre':nombre,
-        'tipo':cultivos['tipo'],
-        'descripcion':cultivos['descripcion'],
-        'siembra':cultivos['siembra'],
-        'cosecha':cultivos['cosecha'],
-    }
-    return render(request, 'cultivos/detalle.html', contexto)
-    
+    pass
